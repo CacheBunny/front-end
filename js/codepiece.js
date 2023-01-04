03ea3c83f4ac1c05f5a0b867f96084f872bdb7fd
cachebunny <imyanhao2020@gmail.com>
2023-01-03 22:30:19 -0500

    add comment

--- js/codepiece.js
+++ js/codepiece.js
@@ -410,3 +410,44 @@ penguin.color; // 'black and white'
 penguin.energy; // 200
 penguin.isActive(); // Energy is decreasing, currently at: 180
 
+
+
+//codepiece17
+
+const car2 = {
+    speed: 200,
+    color: "red"
+}
+console.log(Object.keys(car2)); // ['speed','color']
+
+const car3 = {
+    speed: 300,
+    color: "yellow"
+}
+console.log(Object.values(car3)); // [300, 'yellow']
+
+const car4 = {
+    speed: 400,
+    color: 'magenta'
+}
+console.log(Object.entries(car4));// [ ['speed', 400], ['color', 'magenta'] ]
+
+
+
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for( key of Object.keys(clothingItem) ) {
+    console.log(key, ":", clothingItem[key])
+}
+
+//price : 50
+//color : beige
+//material : cotton
+//season : autumn
+
+//codepiece18

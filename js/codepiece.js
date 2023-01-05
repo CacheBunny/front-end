7514833b99ae7948004edf5b725acafc5b1a830d
cachebunny <imyanhao2020@gmail.com>
2023-01-05 05:51:34 -0500

    add comment

--- js/codepiece.js
+++ js/codepiece.js
@@ -451,3 +451,44 @@ for( key of Object.keys(clothingItem) ) {
 //season : autumn
 
 //codepiece18
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
+
+input.addEventListener('change', function() {
+    h1.innerText = input.value
+})
+
+//cp19
+//https://example.com/
+var h1 = document.querySelector('h1');
+var arr = [
+    'Example Domain',
+    'First Click',
+    'Second Click',
+    'Third Click'
+];
+function handleClicks(){
+    switch(h1.innerText){
+        case arr[0]:
+            h1.innerText=arr[1];
+            break;
+        case arr[1]:
+            h1.innerText=arr[2];
+            break;
+        case arr[2]:
+            h1.innerText=arr[3];
+            break;
+        default:
+            h1.innerText=arr[0];
+            break;
+    }
+}
+
+h1.addEventListener('click',handleClicks);
---
+++ js/test/addFive.test.js
@@ -0,0 +1,7 @@
+const { default: TestRunner } = require("jest-runner");
+// const { default: test } = require("node:test");
+const addFive = require('./addFive');
+
+test('returns the number plus 5',() => {
+    expect(addFive(1)).toBe(6);
+})
